be148f44f4a8da83e55750fd334f3eca7cff1e82
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2026-09-11 18:14:42 -0300

    test: verifica ordem de chegada e independência da saída no desafio 002

--- exercises/2026/09/002-registro-de-inscricoes/test_solution.py
+++ exercises/2026/09/002-registro-de-inscricoes/test_solution.py
@@ -1,10 +1,13 @@
 from solution import organizar_inscricoes
 from copy import deepcopy
 
-emails = [
+emails_repetidos = [
+    "carla@example.com",
     "ana@example.com",
+    "eduardo@example.com",
     "bruno@example.com",
     "ana@example.com",
+    "fernanda@example.com",
     "carla@example.com",
     "bruno@example.com",
     "ana@example.com",
@@ -12,30 +15,68 @@ emails = [
 
 sem_email = []
 
-antes = deepcopy(emails)
-obtido = organizar_inscricoes(emails)
+emails_unicos = [
+    "marcos@example.com",
+    "ana@example.com",
+    "zuleica@example.com",
+    "bruno@example.com",
+    "fernanda@example.com",
+    "carlos@example.com",
+]
 
-# Testar integridade do lista
-assert antes == emails, "Função modifica argumento inplace"
-# Testar resultados unitários
+# Teste email repetidos
+antes = deepcopy(emails_repetidos)
+obtido = organizar_inscricoes(emails_repetidos)
+assert antes == emails_repetidos, "Função modifica argumento inplace"
 assert obtido['inscritos'] == [
-        "ana@example.com",
-        "bruno@example.com",
-        "carla@example.com",
-    ], "A função não retorna a lista de emails correta ou na ordem errada"
-assert obtido['inscritos'][0] == 'ana@example.com', "Retornou primeiro inscrito incorreto"
-assert obtido['inscritos'][-1] == 'carla@example.com', "Retornou último inscrito incorreto"
-assert obtido['repetidas'] == 3, 'Calculou errado  número de emails repetidos'
-# Testar para objeto inteiro
+    "carla@example.com",
+    "ana@example.com",
+    "eduardo@example.com",
+    "bruno@example.com",
+    "fernanda@example.com"
+], "A função não retorna a lista de emails correta ou na ordem errada"
+assert obtido['inscritos'][0] == 'carla@example.com', "Retornou primeiro inscrito incorreto"
+assert obtido['inscritos'][-1] == 'fernanda@example.com', "Retornou último inscrito incorreto"
+assert obtido['repetidas'] == 4, 'Calculou errado  número de emails repetidos'
 assert obtido == {
     "inscritos": [
-        "ana@example.com",
-        "bruno@example.com",
         "carla@example.com",
-    ],
-    "repetidas": 3,
+        "ana@example.com",
+        "eduardo@example.com",
+        "bruno@example.com",
+        "fernanda@example.com"
+],
+    "repetidas": 4,
 }, "Retornou o objeto errado"
+
+# Teste sem emails
 obtido = organizar_inscricoes(sem_email)
 assert obtido == {"inscritos": [], "repetidas": 0}, "Retornou objeto padrão vazio errado"
 assert obtido["inscritos"] == [], "Não retornou lista vazia"
 assert obtido["repetidas"] == 0, "Calculou número diferente de zero para entrada nula"
+
+# Teste email únicos
+antes = deepcopy(emails_unicos)
+obtido = organizar_inscricoes(emails_unicos)
+assert obtido['inscritos'] == [
+    "marcos@example.com",
+    "ana@example.com",
+    "zuleica@example.com",
+    "bruno@example.com",
+    "fernanda@example.com",
+    "carlos@example.com"
+], "Modificou a ordem ou os emails"
+assert obtido['repetidas'] == 0, "Lista não possui email repetidos"
+assert obtido == {
+    "inscritos": [
+        "marcos@example.com",
+        "ana@example.com",
+        "zuleica@example.com",
+        "bruno@example.com",
+        "fernanda@example.com",
+        "carlos@example.com",
+],
+    "repetidas": 0
+}
+obtido["inscritos"].append('leo@example.com')
+assert antes == emails_unicos, "Modificou referência"
